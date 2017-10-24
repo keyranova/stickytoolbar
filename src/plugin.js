8b13b70480489270c526cc0f7641e4e954d78c87
kurtnovack <kurt@walltowall.com>
2017-10-24 17:36:36 -0400

    begins exploring tinymce 4.7.1 changes

--- src/plugin.js
+++ src/plugin.js
@@ -13,63 +13,71 @@ const plugin = (editor) => {
     const container = editor.getContainer();
 
     if (!editor.inline && container && container.offsetParent) {
-      const menubar = container.querySelector('.mce-menubar'),
-        statusbar = container.querySelector('.mce-statusbar'),
-        toolbar = container.querySelector('.mce-toolbar-grp');
+      // const menubar = container.querySelector('.mce-menubar'),
+    //     statusbar = container.querySelector('.mce-statusbar'),
+    //     toolbar = container.querySelector('.mce-toolbar-grp');
+
+      let statusbar = '';
+
+      if (editor.settings.statusbar !== false) {
+        statusbar = container.querySelector('.mce-statusbar');
+      }
+
+      const topPart = container.querySelector('.mce-top-part');
 
       if (isSticky()) {
-        if (menubar) {
-          container.style.paddingTop = `${toolbar.offsetHeight + menubar.offsetHeight}px`;
-        } else {
-          container.style.paddingTop = `${toolbar.offsetHeight}px`;
-        }
+    //     if (menubar) {
+    //       container.style.paddingTop = `${toolbar.offsetHeight + menubar.offsetHeight}px`;
+    //     } else {
+    //       container.style.paddingTop = `${toolbar.offsetHeight}px`;
+    //     }
 
-        if (isAtBottom()) {
-          if (menubar) {
-            menubar.style.top = null;
-            menubar.style.borderBottom = null;
+    //     if (isAtBottom()) {
+    //       if (menubar) {
+    //         menubar.style.top = null;
+    //         menubar.style.borderBottom = null;
 
-            menubar.style.bottom = `${statusbar.offsetHeight + toolbar.offsetHeight}px`;
-            menubar.style.position = 'absolute';
-            menubar.style.width = '100%';
-          }
+    //         menubar.style.width = '100%';
+    //         menubar.style.position = 'absolute';
 
-          toolbar.style.top = null;
-          toolbar.style.borderBottom = null;
+    //         if (statusbar) {
+    //           menubar.style.bottom = `${statusbar.offsetHeight + toolbar.offsetHeight}px`;
+    //         } else {
+    //           menubar.style.bottom = `${toolbar.offsetHeight}px`;
+    //         }
+    //       }
 
-          toolbar.style.bottom = `${statusbar.offsetHeight}px`;
-          toolbar.style.position = 'absolute';
-          toolbar.style.width = '100%';
-        } else {
-          if (menubar) {
-            menubar.style.bottom = null;
+    //       toolbar.style.top = null;
+    //       toolbar.style.borderBottom = null;
 
-            menubar.style.top = `${offset}px`;
-            menubar.style.position = 'fixed';
-            menubar.style.width = `${container.clientWidth}px`;
-            menubar.style.backgroundColor = '#f0f0f0';
-          }
+    //       toolbar.style.width = '100%';
+    //       toolbar.style.position = 'absolute';
 
-          toolbar.style.bottom = null;
+    //       toolbar.style.bottom = statusbar ? `${statusbar.offsetHeight}px` : 0;
+    //     } else {
+    //       if (menubar) {
+    //         menubar.style.bottom = null;
 
-          toolbar.style.top = `${menubar ? (menubar.offsetHeight + offset) : offset}px`;
-          toolbar.style.position = 'fixed';
-          toolbar.style.width = `${container.clientWidth}px`;
-          toolbar.style.borderBottom = '1px solid rgba(0, 0, 0, 0.2)';
-        }
+    //         menubar.style.top = `${offset}px`;
+    //         menubar.style.position = 'fixed';
+    //         menubar.style.width = `${container.clientWidth}px`;
+    //         menubar.style.backgroundColor = '#f0f0f0';
+    //       }
+
+    //       toolbar.style.bottom = null;
+
+    //       toolbar.style.top = `${menubar ? (menubar.offsetHeight + offset) : offset}px`;
+    //       toolbar.style.position = 'fixed';
+    //       toolbar.style.width = `${container.clientWidth}px`;
+    //       toolbar.style.borderBottom = '1px solid rgba(0, 0, 0, 0.2)';
+    //     }
       } else {
         container.style.paddingTop = 0;
 
-        if (menubar) {
-          menubar.style.position = 'relative';
-          menubar.style.top = null;
-          menubar.style.width = null;
-          menubar.style.borderBottom = null;
-        }
-        toolbar.style.position = 'relative';
-        toolbar.style.top = null;
-        toolbar.style.width = null;
-        toolbar.style.borderBottom = null;
+        topPart.style.position = 'relative';
+        topPart.style.top = null;
+        topPart.style.width = null;
+        topPart.style.borderBottom = null;
       }
     }
   }
@@ -85,20 +93,21 @@ const plugin = (editor) => {
   }
 
   function isAtBottom() {
-    const container = editor.getContainer(),
-      editorPosition = container.getBoundingClientRect().top,
-      menubar = container.querySelector('.mce-menubar');
+    // const container = editor.getContainer(),
+    //   editorPosition = container.getBoundingClientRect().top,
+    //   menubar = container.querySelector('.mce-menubar'),
+    //   statusbar = container.querySelector('.mce-statusbar'),
+    //   toolbar = container.querySelector('.mce-toolbar-grp');
 
-    const statusbarHeight = container.querySelector('.mce-statusbar').offsetHeight,
-      toolbarHeight = container.querySelector('.mce-toolbar-grp').offsetHeight;
+    // const menubarHeight = menubar ? menubar.offsetHeight : 0,
+    //   statusbarHeight = statusbar ? statusbar.offsetHeight : 0,
+    //   toolbarHeight = toolbar ? toolbar.offsetHeight : 0;
 
-    const menubarHeight = menubar ? menubar.offsetHeight : 0;
+    // const stickyHeight = -(container.offsetHeight - menubarHeight - statusbarHeight - toolbarHeight);
 
-    const stickyHeight = -(container.offsetHeight - menubarHeight - statusbarHeight - toolbarHeight);
-
-    if (editorPosition < stickyHeight + offset) {
-      return true;
-    }
+    // if (editorPosition < stickyHeight + offset) {
+    //   return true;
+    // }
 
     return false;
   }
